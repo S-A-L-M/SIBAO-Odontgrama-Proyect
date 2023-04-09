37621a965e660a1d3417e829dcaa163d9066cc1a
Santiago L <santiagolozman@gmail.com>
2023-04-08 23:40:27 -0500

    Agrego una base para un API SERVICE DE GMAIL

--- src/config/static/js/script.js
+++ src/config/static/js/script.js
@@ -58,3 +58,46 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+// carga la API de Google
+gapi.load('client', init);
+
+function init() {
+  // inicializa la API de Google con tu CLIENT_ID
+  gapi.client.init({
+    'apiKey': 'AIzaSyAr6-Qevgfy3lAGZ-8SDVjbHAPCmWKk5MQ',
+    'clientId': '887732355759-6qt78dnapfnm1j27b4e34cu6hkcd6fat.apps.googleusercontent.com',
+    'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest'],
+    'scope': 'https://www.googleapis.com/auth/gmail.send'
+  }).then(function () {
+    // cuando la API de Google está lista, llama a la función sendEmail()
+    sendEmail();
+  }, function(error) {
+    // si hay un error al inicializar la API de Google, muestra un mensaje de error en la consola
+    console.error(error);
+  });
+}
+
+function sendEmail() {
+  // obtiene el correo electrónico del usuario del campo de entrada
+  var userEmail = document.getElementById('email-input').value;
+
+  // crea el mensaje de correo electrónico
+  var message = "To: " + userEmail + "\r\n" +
+    "Subject: Confirmación de correo electrónico\r\n\r\n" +
+    "¡Gracias por registrarte! Te contactaremos pronto.";
+
+  // codifica el mensaje en Base64 para que pueda ser enviado a través de la API de Gmail
+  var encodedMessage = btoa(message);
+
+  // llama a la API de Gmail para enviar el correo electrónico
+  var request = gapi.client.gmail.users.messages.send({
+    'userId': 'me',
+    'resource': {
+      'raw': encodedMessage
+    }
+  });
+  request.execute(function(response) {
+    // muestra un mensaje de éxito en la consola
+    console.log(response);
+  });
+}
