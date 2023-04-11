699d2cc0396266b035cc31b63fd1458ee7c7dbce
Santiago L <santiagolozman@gmail.com>
2023-04-11 14:43:04 -0500

    Se agrego un cambio de nueva trayectoria para el proyecto, se elimina rutas del login, ahora el login será el index que se verá como inicial en vez de ser la plantilla informativa, se elimino index, ruta de ello

--- src/app.py
+++ src/app.py
@@ -7,7 +7,6 @@ from api.roles import routes_roles
 
 #rutas
 from rutas.odontograma import routes_odontograma
-from rutas.LoginandRegister import routes_loginandregister
 
 #ubicacion del api
 app.register_blueprint(routes_roles, url_prefix="/api")
@@ -15,11 +14,14 @@ app.register_blueprint(routes_roles, url_prefix="/api")
 
 #ubicacion rutas
 app.register_blueprint(routes_odontograma, url_prefix="/fronted")
-app.register_blueprint(routes_loginandregister, url_prefix="/fronted")
+
+
+
+
 @app.route("/")
 def index():
     titulo= "Pagina Princiapl"
-    return render_template('main/MainOdont.html', titles=titulo)
+    return render_template('main/indexLog.html', titles=titulo)
 
 
 @app.route("/algo")
