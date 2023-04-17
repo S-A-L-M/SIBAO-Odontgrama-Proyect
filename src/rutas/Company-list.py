3b0219b97ea6fc703eea3f8f08ed896813cf7293
Santiago L <santiagolozman@gmail.com>
2023-04-17 06:16:49 -0500

    Agrego rutas para Company y Comapny-list

---
+++ src/rutas/Company-list.py
@@ -0,0 +1,10 @@
+from config.db import db, app, ma
+from flask import Blueprint, Flask,  redirect, request, jsonify, json, session, render_template
+
+routes_companylist = Blueprint("routes_companylist", __name__)
+
+
+@routes_companylist.route('/indexcompanylist', methods=['GET'] )
+def indexcompanylist():
+    
+    return render_template('/main/Company-list.html')
