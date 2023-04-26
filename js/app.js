07805f145345b4212eff7160690aa79c6e1e324b
santyrgh17 <santiagoguereroh034@gmail.com>
2023-04-26 16:31:52 -0500

    ..

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-class Tooth {
+/*class Tooth {
     id;
     element;
     state = []; // length 7
@@ -29,58 +29,9 @@ class Tooth {
     }  
   }
   
-  const tooth1 = new Tooth(1);
-  const tooth2 = new Tooth(2);
-  const tooth3 = new Tooth(3);
-  const tooth4 = new Tooth(4);
-  const tooth5 = new Tooth(5);
-  const tooth6 = new Tooth(6);
-  const tooth7 = new Tooth(7);
-  const tooth8 = new Tooth(8);
-  const tooth9 = new Tooth(9);
-  const tooth10 = new Tooth(10);
-  const tooth11 = new Tooth(11);
-  const tooth12 = new Tooth(12);
-  const tooth13 = new Tooth(13);
-  const tooth14 = new Tooth(14);
-  const tooth15 = new Tooth(15);
-  const tooth16 = new Tooth(16);
-  const tooth17 = new Tooth(17);
-  const tooth18 = new Tooth(18);
-  const tooth19 = new Tooth(19);
-  const tooth20 = new Tooth(20);
-  const tooth21 = new Tooth(21);
-  const tooth22 = new Tooth(22);
-  const tooth23 = new Tooth(23);
-  const tooth24 = new Tooth(24);
-  const tooth25 = new Tooth(25);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
-  const tooth26 = new Tooth(26);
+  const tooth1 = new Tooth(1);*/
+
+
 
 // Obtener todas las partes del diente
 const toothParts = document.querySelectorAll('.tooth-part');
